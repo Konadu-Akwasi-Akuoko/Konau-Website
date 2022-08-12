bffcddf6d5f2a81875d7479734c8aeef1ca4e1db
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-08-12 16:05:28 +0000

    Changed setting of the font size

--- src/pages/Home/components/AboutMe.tsx
+++ src/pages/Home/components/AboutMe.tsx
@@ -14,16 +14,16 @@ function AboutMe() {
       <div className="about__div02">
         <p className="h3-medium">About Me</p>
         <p className="about__div02__p02 h2-bold">Full stack developer</p>
-        <p className="about__div02__p03">
+        <p className="about__div02__p03 p-content">
           Hello! My name is Konadu, and I like making things that are
           interactive and exist online including websites.
         </p>
-        <p className="about__div02__p04">
+        <p className="about__div02__p04 p-content">
           When I decided to attempt and make my own game with Unity3d in 2019,
           that is when my interest in programming first began. It turns out that
           experience taught me a lot about programming.
         </p>
-        <p className="about__div02__p05">
+        <p className="about__div02__p05 p-content">
           I left up game programming when I began my undergraduate studies in
           computer technology because I fell in love with web development. I can
           now create extremely interactive websites with frontend and backend
