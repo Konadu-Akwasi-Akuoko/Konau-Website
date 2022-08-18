7b5c7973af62c1381b2f53a608d0ba656ffe4886
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-08-18 16:55:37 +0000

    Working on work-section

---
+++ src/pages/Home/components/Work.tsx
@@ -0,0 +1,140 @@
+import React from "react";
+
+function Work() {
+  return (
+    <div className="work body__padding-vertical body__padding-horizontal">
+      <p className="h3-medium">Work I&apos;ve Done</p>
+      <span className="h2-bold colored-text">Portfolio</span>
+      <div className="work__div">
+        <div className="work__div__div">
+          <img
+            alt="ALT"
+            src="/playground_assets/4307057%201-1500w.png"
+            className="work__div__div__img"
+          />
+          <div className="work__div__div__div">
+            <div className="work__divX4">
+              <span className="work__divX4__span01 text-3xl">Pricee App</span>
+              <div className="work__divX4__div01">
+                <span className="text-sm work__divX4__div01__span">
+                  Mongo DB
+                </span>
+                <span className="text-sm work__divX4__div01__span">
+                  Node JS
+                </span>
+                <span className="text-sm work__divX4__div01__span">React</span>
+              </div>
+              <span className="work__divX4__span02 p-content">
+                As a frontend developer, I structure content using HTML, style
+                it with CSS, and employ JavaScript when the case calls for more
+                complex interactivity.
+              </span>
+              <div className="work__divX4__div02">
+                <button type="button" className="button-icon">
+                  <a href="/">
+                    <span className="home-text23">Live</span>
+                    <img
+                      alt="ALT"
+                      src="/playground_assets/akar-icons_link-out.svg"
+                      className="home-image04"
+                    />
+                  </a>
+                </button>
+                <div className="home-container04 buttonWithIcons">
+                  <span className="home-text24">Code</span>
+                  <img
+                    alt="ALT"
+                    src="/playground_assets/bx_code-alt.svg"
+                    className="home-image05"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="home-portfolio-card1">
+          <img
+            alt="ALT"
+            src="/playground_assets/4307057%201-1500w.png"
+            className="home-image06"
+          />
+          <div className="home-content-container1">
+            <div className="home-content1">
+              <span className="home-text25 heading2-Bold">Pricee App</span>
+              <div className="home-pills1">
+                <span className="text-Sm pill">Mongo DB</span>
+                <span className="text-Sm pill">Node JS</span>
+                <span className="text-Sm pill">React</span>
+              </div>
+              <span className="home-text29 content">
+                As a frontend developer, I structure content using HTML, style
+                it with CSS, and employ JavaScript when the case calls for more
+                complex interactivity.
+              </span>
+              <div className="home-container05">
+                <div className="home-container06 buttonWithIcons">
+                  <span className="home-text30">Live</span>
+                  <img
+                    alt="ALT"
+                    src="/playground_assets/akar-icons_link-out.svg"
+                    className="home-image07"
+                  />
+                </div>
+                <div className="home-container07 buttonWithIcons">
+                  <span className="home-text31">Code</span>
+                  <img
+                    alt="ALT"
+                    src="/playground_assets/bx_code-alt.svg"
+                    className="home-image08"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="home-portfolio-card2">
+          <img
+            alt="ALT"
+            src="/playground_assets/4307057%201-1500w.png"
+            className="home-image09"
+          />
+          <div className="home-content-container2">
+            <div className="home-content2">
+              <span className="home-text32 heading2-Bold">Pricee App</span>
+              <div className="home-pills2">
+                <span className="pill text-Sm">Mongo DB</span>
+                <span className="text-Sm pill">Node JS</span>
+                <span className="text-Sm pill">React</span>
+              </div>
+              <span className="home-text36 content">
+                As a frontend developer, I structure content using HTML, style
+                it with CSS, and employ JavaScript when the case calls for more
+                complex interactivity.
+              </span>
+              <div className="home-container08">
+                <div className="home-container09 buttonWithIcons">
+                  <span className="home-text37">Live</span>
+                  <img
+                    alt="ALT"
+                    src="/playground_assets/akar-icons_link-out.svg"
+                    className="home-image10"
+                  />
+                </div>
+                <div className="home-container10 buttonWithIcons">
+                  <span className="home-text38">Code</span>
+                  <img
+                    alt="ALT"
+                    src="/playground_assets/bx_code-alt.svg"
+                    className="home-image11"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
